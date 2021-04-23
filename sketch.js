5c409fc3ee4ef5d0c14998ca2b43e412caa5223d
nikeshchavan <nikeshchavan17@gmail.com>
2021-04-23 16:01:26 +0530

    add colliding function

--- sketch.js
+++ sketch.js
@@ -72,7 +72,15 @@ function draw(){
 
 function isColliding(object1,object2){
 // collision Algorithm of 2 boxes and everytime colour changes randomnly on collision
-
+if(object1.x-object2.x < object2.width/2 + object1.width/2 &&
+  object2.x-object1.x < object2.width/2 + object1.width/2 &&
+  object1.y-object2.y < object2.height/2 + object1.height/2 &&
+  object2.y-object1.y <  object2.height/2 + object1.height/2 ){
+    return true
+  }
+  else{
+    return false
+  }
  
 }
 
